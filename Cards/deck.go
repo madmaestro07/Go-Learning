872cdcf023d1180cbdd4cfc34105d236f1f75a61
Saravana Kumar K <saravanan9112000@gmail.com>
2026-04-27 00:31:58 +0530

    completed the blackjack app & started testing

--- Cards/deck.go
+++ Cards/deck.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
 
 func newDeck() deck {
-	cards := deck{} // creating an empty deck of cards
+	cards := deck{}                                                // creating an empty deck of cards
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"} // defining the suits of the cards
-	cardValues := []string{"Ace", "Two", "Three", "Four"} // defining the values of the cards
+	cardValues := []string{"Ace", "Two", "Three", "Four"}          // defining the values of the cards
 
 	for _, suit := range cardSuits { // iterating over the suits of the cards
-		for _,value := range cardValues {
-			cards = append(cards, value + " of " + suit) // appending a new card to the deck by combining the value and suit of the card
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit) // appending a new card to the deck by combining the value and suit of the card
 		}
 	}
 	return cards
 }
 
-func (d deck) print() {  // defining a method called print on the deck type which takes a deck as a receiver and prints the values in the deck
+func (d deck) print() { // defining a method called print on the deck type which takes a deck as a receiver and prints the values in the deck
 	for i, card := range d {
 		fmt.Println(i, card) // printing the index and value of each card in the deck
 	}
@@ -37,4 +39,22 @@ func (d deck) toString() string { // defining a method called toString on the de
 
 func (d deck) saveToFile(filename string) error { // defining a method called saveToFile on the deck type which takes a deck as a receiver and a filename as an argument and saves the deck to a file
 	return os.WriteFile(filename, []byte(d.toString()), 0666) // writing the string representation of the deck to a file with the specified filename and permissions
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename) // reading the contents of the file with the specified filename
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ",")) // converting the file contents to a slice of strings and creating a new deck
+}
+
+func (d deck) shuffle() { // defining a method called shuffle on the deck type which takes a deck as a receiver and shuffles the cards in the deck
+	source := rand.NewSource(time.Now().UnixNano()) // creating a new random source using the process ID to ensure different shuffling each time
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)           // generating a random index for the new position of the card
+		d[i], d[newPosition] = d[newPosition], d[i] // swapping the current card with the card at the new position
+	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -11,5 +11,9 @@ func main() {
 	// hand.print() // printing the hand of cards
 	// remainingCards.print() // printing the remaining cards in the deck
 
-	cards.saveToFile("my_cards") // saving the deck of cards to a file called "my_cards"
+	// cards.saveToFile("my_cards") // saving the deck of cards to a file called "my_cards"
+	// cardsFromFile := newDeckFromFile("my_cards") // creating a new deck of cards by reading from the file "my_cards"
+	// cardsFromFile.print() // printing the deck of cards that was read from the file
+	cards.shuffle() // shuffling the deck of cards
+	cards.print()   // printing the shuffled deck of cards
 }
